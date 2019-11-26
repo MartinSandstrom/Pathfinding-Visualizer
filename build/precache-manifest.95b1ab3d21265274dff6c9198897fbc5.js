68784c680294ef5f727805f07251a882e8d4f3ba
Martin Sandström <martinsandstroms@gmail.com>
2019-11-26 21:27:47 +1100

    Adding drag out wall feature

---
+++ build/precache-manifest.95b1ab3d21265274dff6c9198897fbc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7e2143c8e243fa42e7970990e15f8a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0257767380dede84254b",
+    "url": "./static/css/main.4b7805ed.chunk.css"
+  },
+  {
+    "revision": "af206c65a4a94258a411",
+    "url": "./static/js/2.97e4cbac.chunk.js"
+  },
+  {
+    "revision": "0257767380dede84254b",
+    "url": "./static/js/main.871c39b8.chunk.js"
+  },
+  {
+    "revision": "edd675120b16ecf39f61",
+    "url": "./static/js/runtime-main.3099656e.js"
+  }
+]);
