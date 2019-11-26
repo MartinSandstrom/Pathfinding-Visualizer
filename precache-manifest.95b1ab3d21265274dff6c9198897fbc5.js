134efdec64f71cfbc2638f5dd6d819dcb180c359
Martin Sandström <martinsandstroms@gmail.com>
2019-11-26 21:20:33 +1100

    Updates

--- precache-manifest.95b1ab3d21265274dff6c9198897fbc5.js
+++ precache-manifest.95b1ab3d21265274dff6c9198897fbc5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eb47bfdac1d4ca4243cdb42abe0df2c4",
+    "revision": "3d7e2143c8e243fa42e7970990e15f8a",
     "url": "./index.html"
   },
   {
-    "revision": "cc70a54078754b19dbf1",
+    "revision": "0257767380dede84254b",
     "url": "./static/css/main.4b7805ed.chunk.css"
   },
   {
-    "revision": "6b6315f7142aa6470472",
-    "url": "./static/js/2.ae6e3aa6.chunk.js"
+    "revision": "af206c65a4a94258a411",
+    "url": "./static/js/2.97e4cbac.chunk.js"
   },
   {
-    "revision": "cc70a54078754b19dbf1",
-    "url": "./static/js/main.d266651a.chunk.js"
+    "revision": "0257767380dede84254b",
+    "url": "./static/js/main.871c39b8.chunk.js"
   },
   {
     "revision": "edd675120b16ecf39f61",
