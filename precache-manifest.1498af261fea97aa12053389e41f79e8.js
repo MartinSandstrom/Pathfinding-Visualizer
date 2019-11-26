04c78925b5fc3ee0f8c4c4c55b837cd1c66f3c69
Martin Sandström <martinsandstroms@gmail.com>
2019-11-26 17:13:45 +1100

    Updates

---
+++ precache-manifest.1498af261fea97aa12053389e41f79e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5987328fb41e45255612d7fd354c152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a174cbe0ac4b5afd51a",
+    "url": "/static/css/main.01c08cb3.chunk.css"
+  },
+  {
+    "revision": "a07a9831f16674b19c0a",
+    "url": "/static/js/2.1e8314d7.chunk.js"
+  },
+  {
+    "revision": "2a174cbe0ac4b5afd51a",
+    "url": "/static/js/main.6183ceca.chunk.js"
+  },
+  {
+    "revision": "e7f71a889e85adcb2277",
+    "url": "/static/js/runtime-main.a5880287.js"
+  }
+]);
