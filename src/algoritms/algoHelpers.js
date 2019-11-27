04dc43538038ebf09190f460e20f64c3b72b08da
Martin Sandström <martinsandstroms@gmail.com>
2019-11-27 20:14:30 +1100

    Refactoring

--- src/algoritms/algoHelpers.js
+++ src/algoritms/algoHelpers.js
@@ -1,5 +1,3 @@
-// Backtracks from the finishNode to find the shortest path.
-// Only works when called *after* the dijkstra method above.
 export function getNodesInShortestPathOrder(finishNode) {
     const nodesInShortestPathOrder = [];
     let currentNode = finishNode;
--- src/algoritms/sorting/merge.js
+++ src/algoritms/sorting/merge.js
@@ -1,10 +1,9 @@
 export const mergeSort = (array, allParts = []) => {
     if (array.length < 2) {
-        return { merged: [...array], allParts }; // it means we no longer divide the array
-        // into smaller chunks
+        return { merged: [...array], allParts };
     }
 
-    const left = array.splice(0, array.length / 2); //left part of  the array
+    const left = array.splice(0, array.length / 2);
 
     const leftM = mergeSort(left, allParts);
     const arrayM = mergeSort(array, allParts);
@@ -23,11 +22,9 @@ function merger(left, right) {
     const arr = [];
     while (left.length && right.length) {
         if (left[0] < right[0]) {
-            arr.push(left.shift()); // remove from the left part and push into
-            //the sorted array
+            arr.push(left.shift());
         } else {
-            arr.push(right.shift()); // remove from the right part and push into
-            //the sorted array
+            arr.push(right.shift());
         }
     }
     return [...arr, ...left, ...right];
